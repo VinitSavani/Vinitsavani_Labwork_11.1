32fd4d60a425e2912d5167f6a7098e50f06f18a3
Vinit Savani <136707852+VinitSavani@users.noreply.github.com>
2023-08-01 11:50:29 +0530

    Update vin_Q1.c

--- vin_Q1.c
+++ vin_Q1.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+void main()
+{
+    int a,i,square;
+    
+    printf("Enter array size :- ");
+    scanf("%d",&a);
+    
+    int v[a];
+    
+    int *p;
+    
+    for (i=0;i<a;i++)
+    {
+        printf("v[%d]",i);
+        scanf("%d",&v[i]);
+    }
+    
+    for (i=0;i<a;i++)
+    {
+        square = v[i] * v[i] ;
+        p = & square;
+        printf("%d,",*p);
+    }
+    
+}
