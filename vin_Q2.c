e0ca023464fc174544e6514d6af89b3740956599
Vinit Savani <136707852+VinitSavani@users.noreply.github.com>
2023-08-01 12:02:58 +0530

    Update vin_Q2.c

--- vin_Q2.c
+++ vin_Q2.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
 
+void main()
+{
+    int a,b;
+    
+    printf("Enter the value of x :- ");
+    scanf("%d",&a);
+    
+    printf("Enter the value of y :- ");
+    scanf("%d",&b);
+    
+    printf("Before swapping:\n");
+    printf("x : %d\n",a);
+    printf("y : %d\n",b);
+    
+    printf("After swapping:\n");
+    
+    int *p;
+    int *q;
+    
+    p = & b;
+    q = & a;
+    
+    printf("x : %d\n",*p);
+    printf("y : %d\n",*q);
+    
+}
